bdb4445a4d1e8210f1eef418cc6ada96096310eb
Jacob Keller <jacob.keller@gmail.com>
2022-03-24 01:28:23 -0700

    initial implementation of ActTotalDuration
    ActTotalDuration is a simple plugin for the Advanced Combat Tracker. It
    adds a "TotalDuration" field to encounters and combatant views.
    
    This new field differs from the existing duration in two key ways:
    
     * For combatants view, this tracks the time of the encounter, and not
       the time an individual combatant participated. If a combatant leaves
       combat, its duration field will stop incrementing. However, as long
       as the encounter is tracked by ACT, the total duration will continue
       incrementing.
    
     * For merged encounters, this tracks the total time spent for all
       encounters. This includes any down time between combat. This is the
       main feature of TotalDuration and is what sets it apart from the
       existing duration field.
    
    The TotalDuration field can be useful to help track how long a full
    dungeon, raid, or similar takes to complete.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

---
+++ ActTotalDuration.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+using System.Linq;
+using System;
+
+namespace ACT_Plugin
+{
+    public class ActTotalDuration : IActPluginV1
+    {
+        // The status label that appears in ACT's plugin tab
+        private Label status;
+
+        public void DeInitPlugin()
+        {
+            status.Text = "Plugin Exited";
+        }
+
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            // Track the status label's reference in our private variable
+            status = pluginStatusText;
+
+            var EncounterDurationColumn = new EncounterData.ColumnDef("TotalDuration", false, "VARCHAR", "TotalDuration",
+                (Data) => { return GetDurationString(GetTotalEncounterDuration(Data)); },
+                (Data) => { return GetDurationString(GetTotalEncounterDuration(Data)); }
+                );
+
+            // Add the new column definition for the encounter
+            EncounterData.ColumnDefs.Add("TotalDuration", EncounterDurationColumn);
+
+            var CombatantDurationColumn = new CombatantData.ColumnDef("TotalDuration", false, "VARCHAR", "TotalDuration",
+                (Data) => { return GetDurationString(GetTotalCombatantDuration(Data)); },
+                (Data) => { return GetDurationString(GetTotalCombatantDuration(Data)); },
+                (Left, Right) => { return GetTotalCombatantDuration(Left).CompareTo(GetTotalCombatantDuration(Right)); }
+                );
+
+            CombatantData.ColumnDefs.Add("TotalDuration", CombatantDurationColumn);
+
+            // Validate the new column in the Options tab
+            ActGlobals.oFormActMain.ValidateTableSetup();
+
+            status.Text = "Plugin Started";
+        }
+        private TimeSpan GetTotalEncounterDuration(EncounterData Data)
+        {
+            if (Data.EndTime < Data.StartTime)
+            {
+                return TimeSpan.Zero;
+            }
+            else
+            {
+                return Data.EndTime - Data.StartTime;
+            }
+        }
+
+        private TimeSpan GetTotalCombatantDuration(CombatantData Data)
+        {
+            if (Data.EncEndTime < Data.EncStartTime)
+            {
+                return TimeSpan.Zero;
+            }
+            else
+            {
+                return Data.EncEndTime - Data.EncStartTime;
+            }
+        }
+
+        private String GetDurationString(TimeSpan Duration)
+        {
+            if (Duration.TotalDays >= 1)
+            {
+                return Duration.ToString(@"d\.hh\:mm\:ss\.f");
+            }
+            else if (Duration.TotalHours >= 1)
+            {
+                return Duration.ToString(@"hh\:mm\:ss\.f");
+            }
+            else
+            {
+                return Duration.ToString(@"mm\:ss\.f");
+            }
+        }
+    }
+}
